dab31f8c437dc539bc3429a3b986fec5dac5392b
Shawn Martin-Truesdell <shawn@ESPER.localdomain>
2022-02-04 13:01:28 -0800

    refactor(ui/skilltree): move common structure into UISkillTreeNode type

--- isl/ui/charactersheet/components/skilltree/nodes/species_node.lua
+++ isl/ui/charactersheet/components/skilltree/nodes/species_node.lua
@@ -1,28 +1,26 @@
 --[[
-   UISpeciesNode extends UIComponent to provide types specific rendering
+   UISpeciesNode extends UISkillTreeNode to provide types specific rendering
    instructions for the root Species node of the Skills graph
 ]]
+local PATH = "/isl/ui/charactersheet/components/skilltree/nodes"
+
 require("/scripts/util.lua")
 require("/scripts/questgen/util.lua")
-require("/isl/ui/uicomponent.lua")
-
-local PATH = "/isl/ui/charactersheet/components/skilltree/nodes"
-local Assets = {}
-Assets.background = PATH.."/assets/graph_species.png"
-Assets.icon = PATH.."/assets/graph_species_default.png"
-Assets.mask = PATH.."/assets/graph_species_mask.png"
-
-local DEFAULT_COLOR = "#FFFFFF"
+require(PATH.."/node.lua")
 
 -- Class ----------------------------------------------------------------------
 
-UISpeciesNode = defineSubclass(UIComponent, "UISpeciesNode")()
+UISpeciesNode = defineSubclass(UISkillTreeNode, "UISpeciesNode") {
+   defaultBackground = PATH.."/assets/graph_species.png",
+   defaultIcon = PATH.."/assets/graph_species_default.png",
+   defaultMask = PATH.."/assets/graph_species_mask.png"
+}
 
 -- Constructor ----------------------------------------------------------------
 
 function UISpeciesNode:init(skill, canvas)
-   self.skill = skill
-   self.canvas = canvas
+   UISkillTreeNode.init(self, skill, canvas) -- super()
+
    assert(
       self.skill.type == "species",
       string.format(
@@ -30,54 +28,18 @@ function UISpeciesNode:init(skill, canvas)
          self.skill.type
       )
    )
-   assert(
-      self.canvas ~= nil,
-      "Unable to bind a valid canvas for a Species Node"
-   )
-
-   self.background = skill.background or Assets.background
-   self.has_custom_background = skill.background ~= nil
-
-   self.radius = root.imageSize(self.background)[1] * 0.5
-   self.bounds = Bounds.new(
-      skill.position:translate({ -1 * self.radius, -1 * self.radius }),
-      skill.position:translate({ self.radius, self.radius })
-   )
 end
 
 -- Methods --------------------------------------------------------------------
 
-function UISpeciesNode:draw(drag_state)
-   -- If we're not close enough to the viewable area to be drawn, skip.
+function UISpeciesNode:draw(skilltree_state)
    if not self.canvas.bounds:collides_bounds(self.bounds) then
       return
    end
 
-   local target_position = self.skill.position:translate(drag_state.offset)
+   UISkillTreeNode.draw(self, skilltree_state)
 
-   -- Draw a skill-defined background or the default Species node background
-   self.canvas:drawImage(
-      self.background,
-      target_position,
-      1,
-      DEFAULT_COLOR,
-      true
-   )
-
-   -- Draw a skill-defined icon or the default Species node icon
-   local icon_image = (self.skill.icon or Assets.icon)
-   if not self.has_custom_background then
-      -- If we're using the default background, then we're going to go ahead
-      -- and MASK the icon image so that it doesn't overrun the frame.
-      icon_image = icon_image.."?addmask="..Assets.mask
-   end
-   self.canvas:drawImage(
-      icon_image,
-      target_position,
-      1,
-      DEFAULT_COLOR,
-      true
-   )
+   local target_position = self.skill.position:translate(skilltree_state.offset)
 
    if not self.has_custom_background then
       -- Now, if we're not using a customized graphic we're going to print
@@ -109,15 +71,4 @@ function UISpeciesNode:draw(drag_state)
          }
       )
    end
-end
-
---- Species nodes are circular, and so must calculate mouseovers and clicks
---- based on their circular area rather than using simple rectangular bounds.
-function UISpeciesNode:area_contains_position(position)
-   -- Offset such that skill.positioin is 0,0
-   local relative_point = Point.new(position):translate(self.skill.position:inverse())
-   -- Then distance by the Pythagorean theorum
-   local distance = math.sqrt((relative_point[1]^2)+(relative_point[2]^2))
-
-   return distance <= self.radius
 end
