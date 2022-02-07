bf833f57b7fbef00cdc80b6daa1f671c1ebd7266
Shawn Martin-Truesdell <shawn@ESPER.localdomain>
2022-02-07 09:35:23 -0800

    feat(stats): change stat data mapping from Player properties to currencies
    This is necessary because we actually can't access the player's properties from
    within the status effect that does stat updates on the player's character.

--- isl/stats/stats.lua
+++ isl/stats/stats.lua
@@ -8,46 +8,41 @@ require("/scripts/questgen/util.lua")
 
 ISLPlayerStats = ISLPlayerStats or createClass("Stats")
 
-function ISLPlayerStats:init()
+function ISLPlayerStats:init(entity_id)
+   self.entity_id = entity_id
+   if player and not entity_id then
+      self.entity_id = player.id()
+   end
+
    local data = root.assetJson("/isl/stats/stats.config")
 
    for stat_name, stat_data in pairs(data) do
       -- save configuration from the json file
       self[stat_name] = stat_data
       self[stat_name].current = 0
-      self[stat_name].bonus = 0
    end
 end
 
 function ISLPlayerStats:set_stat(stat_name, new_value)
    new_value = new_value or self[stat_name].defaultValue
-   new_bonus_value = new_bonus_value or self[stat_name].bonus or 0
 
    self[stat_name].current = new_value
-   self[stat_name].bonus = new_bonus_value
 
    return self
 end
 
-function ISLPlayerStats:modify_stat(stat_name, dv, dbv)
+function ISLPlayerStats:modify_stat(stat_name, dv)
    dv = dv or 0
-   dbv = dbv or 0
    self[stat_name].current = (self[stat_name].current or 0) + dv
-   self[stat_name].bonus = (self[stat_name].bonus or 0) + dbv
 
    return self
 end
 
 function ISLPlayerStats:update(_dt)
    local changed = false
-   if not player then
-      ISLLog.debug("Tried to update stats before the `player` was ready")
-      return false
-   else
-      ISLLog.debug("Updating stats")
-   end
+
    for stat_name, _ in pairs(self) do
-      local new_value = player.getProperty(stat_name)
+      local new_value = world.entityCurrency(self.entity_id, stat_name)
       if self[stat_name].current ~= new_value then
          changed = true
          self[stat_name].current = new_value
@@ -57,19 +52,31 @@ function ISLPlayerStats:update(_dt)
 end
 
 function ISLPlayerStats:save()
+   assert(
+      player ~= nil,
+      "Tried to save stats in a context where the `player` object was unavailable"
+   )
    for stat_name, stat_data in pairs(self) do
-      player.setProperty(stat_name, stat_data.current)
+      local last = player.currency(stat_name)
+      local ds = stat_data.current - last
+      if ds > 0 then
+         player.addCurrency(stat_name, ds)
+      elseif ds < 0 then
+         player.consumeCurrency(stat_name, ds)
+      end
    end
 end
 
 function ISLPlayerStats:reset_stat(stat_name)
    self[stat_name].current = self[stat_name].defaultValue
-   self[stat_name].bonus = 0
-   player.setProperty(stat_name, self[stat_name].defaultValue)
+
+   return self
 end
 
 function ISLPlayerStats:reset_stats()
    for stat_name, _ in pairs(self) do
       self:reset_stat(stat_name)
    end
+
+   return self
 end
