ceea08c04fafa7d499fbbd3f802ee5e3cd4b74c5
Shawn Martin-Truesdell <shawn@ESPER.localdomain>
2022-02-13 13:36:43 -0800

    feat(skillpoints): add ISLSkillPoints namespace for utility functions

--- isl/monster/drop_skill_motes.lua
+++ isl/monster/drop_skill_motes.lua
@@ -1,5 +1,15 @@
+--[[
+  A script provided to allow monsters to drop Into Starlight skill motes
+  Add this to `/baseParameters/statusSettings/primaryScriptSources/-` on your monsters
+
+  This is preferred to using treasure pools because of how skill mote scaling works:
+  The amount of motes a monster drops scales not by the level of the monster, but by
+  the RELATIVE level of the player to the monster at the time of death.
+]]
 require("/scripts/util.lua")
 require("/isl/lib/log.lua")
+require("/isl/lib/util.lua")
+require("/isl/player/skill_points.lua")
 
 local no_op = function() end
 local super_init = init or no_op
@@ -15,13 +25,27 @@ function applyDamageRequest(damageRequest)
 
   -- Then check for death
   if not status.resourcePositive("health") then
-    ISLLog.debug(util.tableToString(damageRequest or {}))
-    ISLLog.debug(util.tableToString(player or {}))
-    ISLLog.debug(util.tableToString(("").player or {}))
+    local player_level = ISLSkillPoints.get_effective_level(
+      damageRequest.sourceEntityId
+    )
+    local monster_level = world.callScriptedEntity(entity.id(),"monster.level")
+    local relative_pool_level = 2 + ISLUtil.clamp(
+      -2,
+      2,
+      monster_level - player_level
+    )
+
+    ISLLog.debug(
+      "Kill! P:%f M:%f, R:%f",
+      player_level,
+      monster_level,
+      relative_pool_level
+    )
+
     local res = world.spawnTreasure(
       entity.position(),
       "isl_skillmotepool",
-      world.callScriptedEntity(entity.id(),"monster.level")
+      relative_pool_level
     )
     if not res then
       ISLLog.debug("Something went wrong spawning motes")
