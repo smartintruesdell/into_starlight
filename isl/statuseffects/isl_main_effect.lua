eb3b32be959350a3984b5fecd0314d9022264655
Shawn Martin-Truesdell <shawn@ESPER.localdomain>
2022-02-14 05:59:04 -0800

    refactor: move isl/stat_effects/* to isl/player/stat_effects/*

--- isl/statuseffects/isl_main_effect.lua
+++ isl/statuseffects/isl_main_effect.lua
@@ -3,7 +3,7 @@
    mod are applied to their damage/health/etc.
 ]]
 require("/isl/player/stats/player_stats.lua")
-require("/isl/stat_effects/stat_effects.lua")
+require("/isl/player/stats/player_stats_controller.lua")
 
 function init()
    -- Change the status effect to only check for updates every 30 ticks, which
