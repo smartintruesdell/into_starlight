5c4f596800e1c641eea1311d43886891d98b315d
Shawn Martin-Truesdell <shawn@ESPER.localdomain>
2022-03-16 06:21:35 -0700

    refactor(skillgraph): reorganized skillgraph.lua for easier readability

--- isl/skillgraph/skillgraph.lua
+++ isl/skillgraph/skillgraph.lua
@@ -57,7 +57,7 @@ function ISLSkillGraph:init()
   self.unlocked_perks = StringSet.new()
   self.highlight_path = {}
   self.highlight_cost = 999
-  self.stats = ISLPlayerStats.new()
+  self.stats = nil
 end
 
 function ISLSkillGraph.initialize(player, force)
@@ -97,41 +97,6 @@ function ISLSkillGraph:load_graph(player, path)
   return self
 end
 
-function ISLSkillGraph:load_saved_skills(player)
-  -- First, load any skills from the player property into our "saved skills"
-  self.saved_skills = get_saved_skills(player)
-
-  -- Then, unlock them
-  self:unlock_skills(player, self.saved_skills:to_Vec(), true)
-
-  -- Then, load common "initialSkills" from the graph config (usually just "start")
-  self:unlock_skills(player, self.config.initialSkills.common, true)
-
-  -- Then, load "initialSkills" for the player's species (usually none)
-  self:unlock_skills(
-    player,
-    self.config.initialSkills.species[player.species()] or
-    self.config.initialSkills.species.default,
-    true
-  )
-
-  -- Build available skills data
-  self:build_available_skills()
-
-  -- Save the current graph back to the player (and trigger an update)
-  self:write_skills_to_player(player)
-
-  return self
-end
-
-function ISLSkillGraph:revert(player)
-  refund_skill_points(player, self.unlocked_skills:size() - self.saved_skills:size())
-
-  SkillGraph = nil
-  return ISLSkillGraph.initialize(player)
-end
-
-
 function ISLSkillGraph:load_modules(bindings)
   bindings = bindings or {}
 
@@ -166,105 +131,56 @@ function ISLSkillGraph:load_modules(bindings)
   return self;
 end
 
-function ISLSkillGraph:unlock_skills(player, skill_id_list, force)
+function ISLSkillGraph:load_saved_skills(player)
+  -- First, load any skills from the player property into our "saved skills"
+  self.saved_skills = get_saved_skills(player)
+
+  -- Then, unlock them
+  self:unlock_skills(player, self.saved_skills:to_Vec(), true)
+
+  -- Then, load common "initialSkills" from the graph config (usually just "start")
+  self:unlock_skills(player, self.config.initialSkills.common, true)
+
+  -- Then, load "initialSkills" for the player's species (usually none)
+  self:unlock_skills(
+    player,
+    self.config.initialSkills.species[player.species()] or
+    self.config.initialSkills.species.default,
+    true
+  )
+
+  -- Build available skills data
+  self:build_available_skills()
+
+  -- Save the current graph back to the player (and trigger an update)
+  self:write_skills_to_player(player)
+
+  return self
+end
+
+function ISLSkillGraph:unlock_skill(_player, skill_id)
+  ISLLog.debug("Unlocking skill '%s'", skill_id)
+
+  self.unlocked_skills:add(skill_id)
+
+  if (self.skills[skill_id].type == "perk") then
+    self.unlocked_perks:add(skill_id)
+  end
+
+  return self
+end
+
+function ISLSkillGraph:unlock_skills(player, skill_id_list)
   for _, skill_id in ipairs(skill_id_list or {}) do
     if not self.unlocked_skills:contains(skill_id) then
-      self:unlock_skill(player, skill_id, force)
+      self:unlock_skill(player, skill_id)
     end
-  end
-  if not force then
-    self:build_available_skills()
-    self:apply_skills_to_stats()
   end
 
   return self
 end
 
-function ISLSkillGraph:unlock_skill(player, skill_id, force)
-  local skill_is_affordable =
-    force or player.isAdmin() or get_skill_points(player) > 0
 
-  if skill_is_affordable then
-    ISLLog.debug("Unlocking skill '%s'", skill_id)
-    self.unlocked_skills:add(skill_id)
-    if (self.skills[skill_id].type == "perk") then
-      self.unlocked_perks:add(skill_id)
-    end
-    if not force then
-      spend_skill_points(player, 1)
-    end
-  else
-    ISLLog.debug("skill was unaffordable")
-  end
-
-  if not force then
-    -- Force is used during initialization, but afterwards any
-    -- unlocks should be accompanied by a rebuild of relationships
-    self:build_available_skills()
-    self:apply_skills_to_stats()
-  end
-
-  return self
-end
-
-function ISLSkillGraph:unlock_highlighted_skills(player)
-  for _, skill_id in ipairs(self.highlight_path or {}) do
-    self:unlock_skill(player, skill_id)
-  end
-
-  return self
-end
-
-function ISLSkillGraph:lock_skill(player, skill_id)
-  local function all_unlocked_children_are_supported()
-    -- For each of the children of the specified node,
-    for child_id, _ in pairs(self.skills[skill_id].children) do
-      -- If that child is unlocked, we want to make sure it has
-      -- a valid path back to `start` even if our node were locked.
-      if child_id ~= "start" and self.unlocked_skills:contains(child_id) then
-        local path = find_shortest_path(
-          self,
-          "start",
-          child_id,
-          false,
-          StringSet.new({ skill_id })
-        )
-
-        if not path then return false end
-      end
-    end
-    return true
-  end
-
-  local skill_is_lockable =
-    self.skills[skill_id].type ~= "species" and
-    self.unlocked_skills:contains(skill_id) and
-    all_unlocked_children_are_supported()
-
-  if skill_is_lockable then
-    ISLLog.debug("Locking skill '%s'", skill_id)
-    self.unlocked_skills:remove(skill_id)
-    self.unlocked_perks:remove(skill_id)
-    refund_skill_points(player, 1)
-  end
-
-  self:build_available_skills()
-  self:apply_skills_to_stats()
-
-  return self
-end
-
--- This is the user-facing version of unlock skill
-function ISLSkillGraph:toggle_skill_if_possible(player, skill_id)
-  if self.unlocked_skills:contains(skill_id) then
-    self:lock_skill(player, skill_id)
-    return true
-  elseif self.available_skills:contains(skill_id) then
-    self:unlock_skill(player, skill_id)
-    return true
-  end
-  return false
-end
 
 function ISLSkillGraph:build_back_links()
   -- For each skill,
@@ -303,6 +219,7 @@ function ISLSkillGraph:build_available_skills()
   return self
 end
 
+
 function ISLSkillGraph:write_skills_to_player(player)
   assert(player ~= nil, "Tried to apply the skill graph while the player was nil")
 
@@ -314,50 +231,6 @@ function ISLSkillGraph:write_skills_to_player(player)
   world.sendEntityMessage(player.id(), "isl_skillgraph_updated")
 
   return self;
-end
-
-function ISLSkillGraph.reset_unlocked_skills(player)
-  local prev_skills = player.getProperty(SKILLS_PROPERTY_NAME, {})
-
-  player.addCurrency("isl_skill_point", #prev_skills - 1) -- -1 for 'start'
-  player.setProperty(SKILLS_PROPERTY_NAME, {})
-
-  return ISLSkillGraph.initialize(player, true)
-end
-
-function ISLSkillGraph:apply_skill_to_stats(skill_id)
-  self._get_stat_details_cache = self._get_stat_details_cache or {}
-  local skill = self.skills[skill_id]
-  if not skill then return end
-
-  for stat_name, stat_value in pairs(skill.unlocks.stats or {}) do
-    self._get_stat_details_cache[stat_name] = nil
-    self.stats:modify_stat(stat_name, stat_value)
-  end
-
-  return self
-end
-
-function ISLSkillGraph:apply_skills_to_stats()
-  self.stats = ISLPlayerStats.new()
-  for _, skill_id in ipairs(self.unlocked_skills:to_Vec()) do
-    self:apply_skill_to_stats(skill_id)
-  end
-
-  return self
-end
-
---TODO: I think I need to rework this in a "now I use stock stats world"
-function ISLSkillGraph:get_stat_details(stat_name)
-  assert(stat_name ~= nil, "Tried to retrieve stat details for `nil`")
-
-  self._get_stat_details_cache[stat_name] = {
-    from_skills = 0,
-    from_perks = 0,
-    from_species = 0
-  }
-
-  return self._get_stat_details_cache[stat_name]
 end
 
 
@@ -381,6 +254,90 @@ function ISLSkillGraph:remove_status_effects_from_player(_--[[Player]])
   end
 end
 
+-- UI Methods -----------------------------------------------------------------
+
+function ISLSkillGraph:user_unlock_skill(player, skill_id)
+  local skill_is_affordable =
+    player.isAdmin() or get_skill_points(player) > 0
+
+  if skill_is_affordable then
+    spend_skill_points(player, 1)
+    self:unlock_skill(player, skill_id)
+    self:build_available_skills()
+  end
+
+  return self
+end
+
+
+function ISLSkillGraph:user_unlock_skills(player, skill_ids)
+  local skills_are_affordable =
+    player.isAdmin() or get_skill_points(player) >= #skill_ids
+
+  if skills_are_affordable then
+    spend_skill_points(player, #skill_ids)
+    self:unlock_skills(player, skill_ids)
+    self:build_available_skills()
+  end
+
+  return self
+end
+
+
+function ISLSkillGraph:user_lock_skill(player, skill_id)
+  local function all_unlocked_children_are_supported()
+    -- For each of the children of the specified node,
+    for child_id, _ in pairs(self.skills[skill_id].children) do
+      -- If that child is unlocked, we want to make sure it has
+      -- a valid path back to `start` even if our node were locked.
+      if child_id ~= "start" and self.unlocked_skills:contains(child_id) then
+        local path = find_shortest_path(
+          self,
+          "start",
+          child_id,
+          false,
+          StringSet.new({ skill_id })
+        )
+
+        if not path then return false end
+      end
+    end
+    return true
+  end
+
+  local skill_is_lockable =
+    self.skills[skill_id].type ~= "species" and
+    self.unlocked_skills:contains(skill_id) and
+    all_unlocked_children_are_supported()
+
+  if skill_is_lockable then
+    ISLLog.debug("Locking skill '%s'", skill_id)
+    self.unlocked_skills:remove(skill_id)
+    self.unlocked_perks:remove(skill_id)
+    refund_skill_points(player, 1)
+
+    self:build_available_skills()
+  end
+
+  return self
+end
+
+-- This is the user-facing version of unlock skill
+function ISLSkillGraph:user_toggle_skill_if_possible(player, skill_id)
+  if self.unlocked_skills:contains(skill_id) then
+    self:user_lock_skill(player, skill_id)
+
+    return true
+  elseif self.available_skills:contains(skill_id) then
+    self:user_unlock_skill(player, skill_id)
+
+    return true
+  end
+
+  return false
+end
+
+
 function ISLSkillGraph:highlight_path_to_skill(goal_id)
   local path, cost = find_shortest_path(
     self,
@@ -394,9 +351,67 @@ function ISLSkillGraph:highlight_path_to_skill(goal_id)
   return self
 end
 
+
 function ISLSkillGraph:clear_highlight_path()
   self.highlight_path = nil
   self.highlight_cost = 999
 end
 
-return ISLSkillGraph
+
+function ISLSkillGraph:unlock_highlighted_skills(player)
+  return self:user_unlock_skills(player, self.highlight_path)
+end
+
+
+function ISLSkillGraph:revert(player)
+  refund_skill_points(player, self.unlocked_skills:size() - self.saved_skills:size())
+
+  return ISLSkillGraph.initialize(player, true)
+end
+
+
+function ISLSkillGraph.reset_unlocked_skills(player)
+  local prev_skills = player.getProperty(SKILLS_PROPERTY_NAME, {})
+
+  player.addCurrency("isl_skill_point", #prev_skills - 1) -- -1 for 'start'
+  player.setProperty(SKILLS_PROPERTY_NAME, {})
+
+  return ISLSkillGraph.initialize(player, true)
+end
+
+
+-- DEAD CODE ------------------------------------------------------------------
+
+--TODO: Dead code, maybe repurpose?
+function ISLSkillGraph:apply_skill_to_stats(skill_id)
+  local skill = self.skills[skill_id]
+  if skill then
+    for stat_name, stat_value in pairs(skill.unlocks.stats or {}) do
+      self._get_stat_details_cache[stat_name] = nil
+      self.stats:modify_stat(stat_name, stat_value)
+    end
+  end
+
+  return self
+end
+
+
+--TODO: Dead code, maybe repurpose?
+function ISLSkillGraph:apply_skills_to_stats(player)
+  self.stats = ISLPlayerStats.new(player.id())
+  for _, skill_id in ipairs(self.unlocked_skills:to_Vec()) do
+    self:apply_skill_to_stats(skill_id)
+  end
+
+  return self
+end
+
+
+--TODO: Deprecate
+function ISLSkillGraph:get_stat_details()
+  return {
+    from_skills = 0,
+    from_perks = 0,
+    from_species = 0
+  }
+end
